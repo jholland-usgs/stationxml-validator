6d2a5373be0662594fe3c019c91ab36eca060e01
yazan <yazan@iris.edu>
2016-03-01 12:37:57 -0800

    Structure reorg

--- src/main/java/edu/iris/dmc/validation/ValidatorService.java
+++
@@ -1,9 +0,0 @@
-package edu.iris.dmc.validation;
-
-import java.util.List;
-
-import edu.iris.dmc.fdsn.station.model.Network;
-
-public interface ValidatorService {
-	public void run(List<Network> list, LEVEL level, Errors errors);
-}
