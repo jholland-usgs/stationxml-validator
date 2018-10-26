e4b322df199cc5d501f66e3cd45c010cc53101a4
TRonan <tronan@iris.washington.edu>
2018-10-26 09:20:19 -0700

    Updated the version of the stationxml-seed-converter in pom.xml

---
+++ src/test/java/edu/iris/dmc/station/conditions/Condition412Test.java
@@ -0,0 +1,60 @@
+package edu.iris.dmc.station.conditions;
+
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.iris.dmc.DocumentMarshaller;
+import edu.iris.dmc.fdsn.station.model.Channel;
+import edu.iris.dmc.fdsn.station.model.FDSNStationXML;
+import edu.iris.dmc.fdsn.station.model.Network;
+import edu.iris.dmc.fdsn.station.model.Station;
+import edu.iris.dmc.station.RuleEngineServiceTest;
+import edu.iris.dmc.station.rules.Message;
+
+public class Condition412Test {
+
+	private FDSNStationXML theDocument;
+
+	@Before
+	public void init() throws Exception {
+
+	}
+
+	@Test
+	public void fail() throws Exception {
+		try (InputStream is = RuleEngineServiceTest.class.getClassLoader().getResourceAsStream("F1_412.xml")) {
+			theDocument = DocumentMarshaller.unmarshal(is);
+
+			Network n = theDocument.getNetwork().get(0);
+			Station s = n.getStations().get(0);
+			Channel c = s.getChannels().get(0);
+			StageGainProductCondition condition = new StageGainProductCondition(true, "");
+
+			Message result = condition.evaluate(c);
+
+			Assert.assertTrue(result instanceof edu.iris.dmc.station.rules.Error);
+			Assert.assertEquals("Product of stage gains 80000.0 must equal total gain 10.0",result.getDescription());
+		}
+
+	}
+
+	@Test
+	public void pass() throws Exception {
+		try (InputStream is = RuleEngineServiceTest.class.getClassLoader().getResourceAsStream("pass.xml")) {
+			theDocument = DocumentMarshaller.unmarshal(is);
+
+			Network n = theDocument.getNetwork().get(0);
+			Station s = n.getStations().get(0);
+			Channel c = s.getChannels().get(0);
+			StageGainProductCondition condition = new StageGainProductCondition(true, "");
+
+			Message result = condition.evaluate(c);
+			
+			Assert.assertTrue(result instanceof edu.iris.dmc.station.rules.Success);
+		}
+
+	}
+}
