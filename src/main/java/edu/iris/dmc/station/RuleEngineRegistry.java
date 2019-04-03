444a72033592a7de20d70bd68d00f05f16e8c365
timronan <tronan@iris.washington.edu>
2019-04-03 15:38:39 -0700

    Update validator rules 101, 201, and 301 to uppercase only. 301 3
    characters

--- src/main/java/edu/iris/dmc/station/RuleEngineRegistry.java
+++ src/main/java/edu/iris/dmc/station/RuleEngineRegistry.java
@@ -75,7 +75,7 @@ public class RuleEngineRegistry {
 	}
 
 	private void defaultNetworkRules(List<Integer> ignoreRules) {
-		String codeRegex = "[A-Za-z0-9\\*\\?]{1,2}";
+		String codeRegex = "[A-Z0-9_\\*\\?]{1,2}";
 		add(101, new CodeCondition(true, codeRegex,
 				"Network:Code must be assigned a string consisting of 1-2 uppercase characters A-Z and or numeric characters 0-9."),
 				Network.class);
@@ -90,7 +90,7 @@ public class RuleEngineRegistry {
 	}
 
 	private void defaultStationRules(List<Integer> ignoreRules) {
-		String codeRegex = "[A-Za-z0-9\\*\\?]{1,5}";
+		String codeRegex = "[A-Z0-9_\\*\\?]{1,5}";
 		add(201, new CodeCondition(true, codeRegex,
 				"Station:Code must be assigned a string consisting of 1-5 uppercase characters A-Z and or numeric characters 0-9."),
 				Station.class);
@@ -116,10 +116,10 @@ public class RuleEngineRegistry {
 	}
 
 	private void defaultChannelRules(List<Integer> ignoreRules) {
-		String codeRegex = "[A-Za-z0-9\\*\\?]{1,3}";
+		String codeRegex = "[A-Z0-9_\\*\\?]{3}";
 		Restriction[] restrictions = new Restriction[] { new ChannelCodeRestriction(), new ChannelTypeRestriction() };
 		add(301, new CodeCondition(true, codeRegex,
-				"Channel:Code must be assigned a string consisting of 1-3 uppercase characters A-Z and or numeric characters 0-9."),
+				"Channel:Code must be assigned a string consisting of 3 uppercase characters A-Z and or numeric characters 0-9."),
 				Channel.class);
 		add(302, new LocationCodeCondition(true, "([A-Za-z0-9\\*\\ ]{0,2})?",
 				"Channel:locationCode must be unassigned or be assigned a string consisting of 1-3 uppercase characters A-Z and or numeric characters 0-9."),
