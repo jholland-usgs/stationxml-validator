31fdcabcf7065f6689190f44e9080e3a5a5a84b4
timronan <tronan@iris.washington.edu>
2019-09-16 15:48:36 -0700

    Remove condtions 330 and 331 makes sure conditions are removed.

--- src/main/java/edu/iris/dmc/station/RuleEngineRegistry.java
+++ src/main/java/edu/iris/dmc/station/RuleEngineRegistry.java
@@ -160,15 +160,7 @@ public class RuleEngineRegistry {
 					"Channel:startDate is required and must occur before Channel:endDate if Channel:endDate is available."),
 					Channel.class);
 		}
-
-		if (!set.contains(330)) {
-			add(330, new AzimuthCondition(true, "Azimuth must be assigned a value between 0 and 360.", 0, 360),
-					Channel.class);
-		}
-		if (!set.contains(331)) {
-			add(331, new DipCondition(true, "Dip must be assigned a value between -90 and 90.", -90, 90),
-					Channel.class);
-		}
+		
 		if (!set.contains(332)) {
 			add(332, new OrientationCondition(true,
 					"Channel:Azimuth and or Channel:Dip do not correspond within 5 degrees of tolerance to last digit of orthogonal Channel:Code.",
--- src/test/java/edu/iris/dmc/station/conditions/Condition330Test.java
+++ src/test/java/edu/iris/dmc/station/conditions/Condition330Test.java
@@ -2,10 +2,16 @@ package edu.iris.dmc.station.conditions;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import javax.xml.bind.UnmarshalException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import org.xml.sax.SAXParseException;
 
 import edu.iris.dmc.DocumentMarshaller;
 import edu.iris.dmc.fdsn.station.model.Channel;
@@ -13,7 +19,7 @@ import edu.iris.dmc.fdsn.station.model.FDSNStationXML;
 import edu.iris.dmc.fdsn.station.model.Network;
 import edu.iris.dmc.fdsn.station.model.Station;
 import edu.iris.dmc.station.RuleEngineServiceTest;
-import edu.iris.dmc.station.conditions.AzimuthCondition;
+import edu.iris.dmc.station.conditions.DipCondition;
 import edu.iris.dmc.station.conditions.EpochRangeCondition;
 import edu.iris.dmc.station.rules.Message;
 
@@ -27,19 +33,11 @@ public class Condition330Test {
 	}
 
 	@Test
-	public void zeroAzimuth() throws Exception {
-		try (InputStream is = RuleEngineServiceTest.class.getClassLoader().getResourceAsStream("P1_330.xml")) {
-			theDocument = DocumentMarshaller.unmarshal(is);
-
-			Network n = theDocument.getNetwork().get(0);
-			Station s = n.getStations().get(0);
-			Channel c = s.getChannels().get(0);
-
-			AzimuthCondition condition = new AzimuthCondition(true, "", 0, 360);
-
-			Message result = condition.evaluate(c);
-			assertTrue(result instanceof edu.iris.dmc.station.rules.Success);
+	public void throwsExceptionWithSpecificType() throws Exception {
+		try (InputStream is = RuleEngineServiceTest.class.getClassLoader().getResourceAsStream("F1_330.xml")) {
+			Assertions.assertThrows(UnmarshalException.class,() -> DocumentMarshaller.unmarshal(is));
 		}
+		
 
 	}
 
